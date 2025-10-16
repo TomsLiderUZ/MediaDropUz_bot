aa8ecb8a80a441972516a1541bda7bfa82eceb35
ItzToms <152501142+TomsLiderUZ@users.noreply.github.com>
2025-10-16 16:54:05 +0500

    Update BOT_TOKEN in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config/config.js
 module.exports = {
-  BOT_TOKEN: '8061996397:AAHI4L__P7KwIvaJJkzzukCUWTqTYHofmB0' // Bu yerga haqiqiy tokeningizni yozing
+  BOT_TOKEN: '8061996397:AAGV8evlOPsXorLCVEWAt4Y34Evhf_lDa7U' // Bu yerga haqiqiy tokeningizni yozing
 };
